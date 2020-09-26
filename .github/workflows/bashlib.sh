5de2bb4beee4f2098e0db07e0bd41b8cf2c912aa
Samuel Wanjiku <samuel@samuelmwangi.co.ke>
2020-09-26 10:37:27 +0300

    Test 2

---
+++ .github/workflows/bashlib.sh
@@ -0,0 +1,15 @@
+default-setup-command() {
+  pip-install
+}
+
+pip-install() {
+  cd $GITHUB_WORKSPACE
+
+  cache-restore pip
+
+  echo "::group::pip install"
+  pip3 install molecule docker yamllint ansible-lint
+  echo "::endgroup::"
+
+  cache-save pip
+}
